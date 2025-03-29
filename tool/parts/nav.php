fcd23fb13efecdd97be626c18280bcc0ff3fd2a5
Derick Ablong <derickablong@gmail.com>
2025-03-29 23:15:48 +0800

    Improving step navigation

--- tool/parts/nav.php
+++ tool/parts/nav.php
@@ -6,11 +6,18 @@
     <div class="step-nav">
         <div class="buttons">
             <button class="prev">
-                <span class="icon"></span>
+                <span class="icon">
+                    <svg id="Lager_1" style="enable-background:new -265 388.9 64 64;" version="1.1" viewBox="-265 388.9 64 64" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M-247.4,420.9l10.1,15.5c0.2,0.3,0.5,0.5,0.8,0.5h7.3c0.7,0,1.2-0.7,0.8-1.4l-6.5-9.9h15.4c0.5,0,0.9-0.4,0.9-0.9v-7.5  c0-0.5-0.4-0.9-0.9-0.9h-15.4l6.5-9.9c0.3-0.6-0.1-1.4-0.8-1.4h-7.3c-0.4,0-0.7,0.2-0.8,0.5L-247.4,420.9z"/></svg>
+                </span>
                 <span>Back</span>
             </button>
             <button class="start-over">Start Over</button>
-            <button class="next">Next</button>
+            <button class="next">                
+                <span>Next</span>
+                <span class="icon">
+                    <svg id="Lager_1" style="enable-background:new -265 388.9 64 64;" version="1.1" viewBox="-265 388.9 64 64" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M-216.3,421.9l-10.7-16.4c-0.2-0.3-0.5-0.6-0.9-0.6h-7.8c-0.7,0-1.2,0.8-0.9,1.4l7,10.6H-246c-0.6,0-1,0.4-1,1v8  c0,0.6,0.4,1,1,1h16.4l-6.9,10.6c-0.3,0.7,0.2,1.4,0.9,1.4h7.8c0.4,0,0.7-0.2,0.9-0.6L-216.3,421.9z"/></svg>
+                </span>
+            </button>
         </div>        
     </div>
 </div>
