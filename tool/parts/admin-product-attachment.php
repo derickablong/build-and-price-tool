d5bd551b3df16a568515b91fbf7276b5f7b02747
Derick Ablong <derickablong@gmail.com>
2025-05-25 16:24:55 +0800

    Created product attachment management

--- tool/parts/admin-product-attachment.php
+++ tool/parts/admin-product-attachment.php
@@ -42,7 +42,7 @@ $loaded_products = [];
 ?>
 
 <div class="wrap">
-    <h1>Product Attachment for model <?php echo $model['title'] ?></h1>    
+    <h1>Product attachment for model <?php echo $model['title'] ?></h1>    
     <div class="attachment-setup">
         <?php
         $result_one = new WP_Query($products_one);
@@ -71,28 +71,56 @@ $loaded_products = [];
             wp_reset_query();
         endif;
         ?>
-        
-
-        <div class="row-attachment">
-            <div class="col">
-                <div class="title">Product</div>
-                <select name="attachment-item[]" class="attachment-item">
-                    <option value="">--- Select Product ---</option>
-                    <?php foreach ($attachments as $product_id => $product): ?>
-                        <option value="<?php echo $product_id ?>"><?php echo $product ?></option>
-                    <?php endforeach; ?>
-                </select>
+        <form action="" method="post">
+            <input type="hidden" name="model-attachment" value="1">            
+            <div class="row-attachment group">                
+                <div class="col">       
+                    <div class="title">Attachment Group</div>         
+                    <select name="attachment-require[]" class="attachment-require" multiple="multiple">
+                        <option value="">--- Select Product ---</option>
+                        <?php foreach ($attachments as $product_id => $product): ?>
+                            <option value="<?php echo $product_id ?>"><?php echo $product ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                <div class="col buttons">
+                    <div class="cta">
+                        <a href="#" class="remove">
+                            <svg height="30px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" width="30px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M256,32C132.3,32,32,132.3,32,256s100.3,224,224,224s224-100.3,224-224S379.7,32,256,32z M384,272H128v-32h256V272z"/></g></svg>
+                        </a>
+                        <a href="#" class="add">
+                            <svg height="30px" id="Layer_1" style="enable-background:new 0 0 512 512;" version="1.1" viewBox="0 0 512 512" width="30px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><path d="M256,32C132.3,32,32,132.3,32,256s100.3,224,224,224s224-100.3,224-224S379.7,32,256,32z M384,272H272v112h-32V272H128v-32   h112V128h32v112h112V272z"/></g></svg>
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="col">
-                <div class="title">Required Attachment</div>
-                <select name="attachment-require[]" class="attachment-require">
-                    <option value="">--- Select Product ---</option>
-                    <?php foreach ($attachments as $product_id => $product): ?>
-                        <option value="<?php echo $product_id ?>"><?php echo $product ?></option>
-                    <?php endforeach; ?>
-                </select>
-            </div>
-        </div>
 
+            <div class="submit">
+                <a href="<?php echo admin_url('/admin.php?page=bpt') ?>" class="button black">Back</a>
+                <button class="button">Save Settings</button>
+            </div>
+        </form>
     </div>
 </div>
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/select2/3.4.8/select2.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/select2/3.4.8/select2.js"></script>
+<script type="text/javascript">
+jQuery(document).ready(function($) {
+    $('.row-attachment select').select2();
+
+    $(document).on('click', '.add', function(e) {
+        e.preventDefault();
+
+        const $item     = $(this).closest('.row-attachment');
+        let   $new_item = $item.clone();
+
+        $new_item.find('.select2-container').remove();
+        $new_item.insertAfter($item);        
+        $new_item.find('select').select2();
+    });
+
+    $(document.body).on("change", ".attachment-item",function(){
+        console.log(this.value);
+    });
+});
+</script>
